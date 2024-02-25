dbcd4486057ef5c06689635b415c05f8ed4fa927
Chideraa <chideraaozobu@gmail.com>
2024-02-25 16:14:51 +0200

    api 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,11 +7,13 @@ import sys
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
-    todolist = requests.get(api_url + "todos", params={"userId": sys.argv[1]}).json()
+    todolist = requests.get(
+            api_url + "todos", params={"userId": sys.argv[1]}).json()
 
-    #uses list comprehension and adds to tc if condition are met
-    tasks_complete = [tc.get("title") for tc in todolist if tc.get("completed") is True]
+    # uses list comprehension and adds to tc if condition are met
+    tasks_complete = [tc.get(
+        "title") for tc in todolist if tc.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("namw"), len(tasks_complete), len(todolist)))
-    for i in range(len(tasks_complete)):
-        print("  {}".format(task_complete[i]))
+        user.get("name"), len(tasks_complete), len(todolist)))
+    for i in tasks_complete:
+        print("  {}".format(i))
