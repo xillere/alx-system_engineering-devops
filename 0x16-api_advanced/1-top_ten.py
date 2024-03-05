bcf6caf279d0c95a0c68be3fab41fa7c9c805591
Chideraa <chideraaozobu@gmail.com>
2024-03-05 12:18:00 +0200

    subreddit api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""print hot posts on a subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """print headings of the top 10 hot posts on provided subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
